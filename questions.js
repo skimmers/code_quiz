16e61ced19ef41e9d48525b859fd87ca2e676173
Sharon Kim <skimilk19@gmail.com>
2021-01-14 07:54:01 -0500

    added questions in js file

---
+++ questions.js
@@ -0,0 +1,17 @@
+var questions = [
+    {
+        title: "What does HTML stand for?",
+        choices: ["Hype TV Man Living", "Hypertext Makup Language", "Hyper Texting Mail Language"],
+        answer: "Hypertext Markup Language",
+    },
+    {
+        title: "What does CSS stand for?",
+        choices: ["Cascading Style Sheets", "Cascading Singing Songs", "Cats Sing Songs"],
+        answer: "Cascading Style Sheets",
+    },
+    {
+        title: "Can you write CSS internally or externally of a HTML file?",
+        choices: ["Internally", "Externally", "Both"],
+        answer: "Internally",
+    },
+];
